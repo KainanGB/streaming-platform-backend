5a7deeadf30ecb3457f010e1771f24db6f66d871
KainanGB <kainanytbr@gmail.com>
2023-08-14 20:59:50 -0300

    chore: initial config

--- src/@types/user.ts
+++ src/@types/user.ts
@@ -1,5 +1,5 @@
 export interface IUser {
-  username: string
+  username?: string
   password: string
   email: string
 }
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,18 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import { usersRouter } from './routes/users-routes'
+import { authenticateRoutes } from './routes/authenticate'
+import { refreshRoutes } from './routes/refresh'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export const app = express()
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cookieParser())
 app.use(express.json())
+app.use(refreshRoutes)
+app.use(authenticateRoutes)
+
+app.use(verifyJWT)
 app.use(usersRouter)
